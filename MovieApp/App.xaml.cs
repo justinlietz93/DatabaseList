f715232472036e3629c0363d53df48eadbc76b32
Justin Lietz <jlietz93@gmail.com>
2024-10-05 21:25:27 -0700

    Created app

---
+++ MovieApp/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MovieApp.Models;
+
+namespace MovieApp;
+
+public partial class App : Application
+{
+    public static List<Movie> MovieList;
+    
+    public App()
+    {
+        InitializeComponent();
+        MovieList = new List<Movie>();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MovieApp/Views/FindPage.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Views;
+
+public partial class FindPage : ContentPage
+{
+    public FindPage()
+    {
+        InitializeComponent();
+        Title = "List Movies";
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var movieTemplate = new DataTemplate(typeof(TextCell));
+        movieTemplate.SetBinding(TextCell.TextProperty,"Title");
+        movieTemplate.SetBinding(TextCell.DetailProperty,"Rating");
+
+        lstMovies.ItemTemplate = movieTemplate;
+        lstMovies.ItemsSource = App.MovieList;
+    }
+}
